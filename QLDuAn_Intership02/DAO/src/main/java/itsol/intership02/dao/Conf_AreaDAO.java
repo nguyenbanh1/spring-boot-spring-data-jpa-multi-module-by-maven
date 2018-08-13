4700745ad1b8d0c0964a8c1a0fd323b06213bde1
alipas10 <phuonkpdd@gmail.com>
2018-08-13 11:18:15 +0700

    thêm lớp dao cho conf_area + media

---
+++ QLDuAn_Intership02/DAO/src/main/java/itsol/intership02/dao/Conf_AreaDAO.java
@@ -0,0 +1,6 @@
+package itsol.intership02.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Conf_AreaDAO extends JpaRepository<Conf_AreaDAO,Integer> {
+}
---
+++ QLDuAn_Intership02/DAO/src/main/java/itsol/intership02/dao/MediaDAO.java
@@ -0,0 +1,7 @@
+package itsol.intership02.dao;
+import itsol.intership02.Entities.MEDIA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MediaDAO extends JpaRepository<MEDIA,Integer> {
+}
