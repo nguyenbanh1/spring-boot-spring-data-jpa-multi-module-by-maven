06712e6f21de8ba2861104658a66b02e370709a4
master <chinguyenxt@gmail.com>
2018-08-15 15:49:08 +0700

    ConfArea chinguyen

---
+++ QLDuAn_Intership02/web-service/src/main/java/itsol/intership02/rescontroller/ConfAreaController.java
@@ -0,0 +1,35 @@
+package itsol.intership02.rescontroller;
+
+import itsol.intership02.Entities.Conf_Area;
+import itsol.intership02.dao.ConfAreaDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class ConfAreaController {
+    @Autowired
+    private ConfAreaDAO confAreaDAO;
+
+
+
+    //them ConfArea chinguyen 15/8
+    @RequestMapping(value = "area/post",method = RequestMethod.POST,produces = {"application/json"})
+    public void postConfArea(@RequestBody Conf_Area newConfArea){//,@RequestHeader String code){
+
+        //Staff staff_create = staffDAO.findByStaffcode(code).orElse(new Staff());
+        Date date = new Date();
+
+        newConfArea.setDate_created(date);
+        //   newConfArea.setUser_created(staff_create);
+        newConfArea.setStatus(true);
+
+        confAreaDAO.save(newConfArea);
+    };
+
+
+}
