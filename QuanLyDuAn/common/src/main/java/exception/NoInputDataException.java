6ef87db380f04d49cc8ad12dc5cc059705b489d9
alipas10 <phuonkpdd@gmail.com>
2018-08-28 15:26:55 +0700

    commit

--- QuanLyDuAn/common/src/main/java/exception/NoInputDataException.java
+++ QuanLyDuAn/common/src/main/java/exception/NoInputDataException.java
@@ -1,5 +1,10 @@
 package exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "No input data exception")
 public class NoInputDataException extends RuntimeException{
     public NoInputDataException(String msg){super(msg);}
 
