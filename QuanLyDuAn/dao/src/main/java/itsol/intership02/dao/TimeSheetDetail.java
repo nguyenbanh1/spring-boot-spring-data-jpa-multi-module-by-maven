067d09e11b431a207579f25832e95ff1293cf36f
nguyenbanh1 <tangkhanhnguyen98@gmail.com>
2018-08-30 11:59:57 +0700

    "nguyen nho"

---
+++ QuanLyDuAn/dao/src/main/java/itsol/intership02/dao/TimeSheetDetail.java
@@ -0,0 +1,6 @@
+package itsol.intership02.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeSheetDetail extends JpaRepository<TimeSheetDetail,Integer> {
+}
---
+++ QuanLyDuAn/dao/src/main/java/itsol/intership02/dao/TimseSheet.java
@@ -0,0 +1,8 @@
+package itsol.intership02.dao;
+
+import itsol.intership02.entities.Timesheet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimseSheet extends JpaRepository<Timesheet,Integer> {
+
+}
